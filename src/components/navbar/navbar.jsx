90a8d50e9029a2f6fadffad92741de6af00d09e2
jayteemoney <jethroirmiya@gmail.com>
2025-03-28 11:34:33 +0100

    modified navbar.css

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -6,9 +6,16 @@ import './navbar.css'
 const Navbar = () => {
   return (
     <div className='gpt3__navbar'>
-      <div className='gpt3__-links'>
+      <div className='gpt3__navbar-links'>
         <div className='gpt3__navbar-links_logo'>
           <img src={GPT} alt="GPT" />
+        </div>
+        <div className='gpt3__navbar-links_container'>
+          <p><a href="#home"></a>Home</p>
+          <p><a href="#wgpt3"></a>What is GPT?</p>
+          <p><a href="#possibility"></a>Open AI</p>
+          <p><a href="#features"></a>Case Studies</p>
+          <p><a href="#blog"></a>Library</p>
 
         </div>
 
