f6edc1f43b33f5acdc3143d01f1646bbc2f49adb
jayteemoney <jethroirmiya@gmail.com>
2025-03-28 04:11:36 +0100

    changes to app.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
+import './App.css';
+import {Blog, Features, Footer, Head, Possibility, WhatGPT3} from './containers'
+import { Cta, Brand, Navbar} from './components'
 
 const App = () => {
   return (
-    <div>
-      <h1>GPT-3</h1>
-    </div>
-  )
-}
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Head />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility/>
+      <Cta />
+      <Blog />
+      <Footer />
 
-export default App
+    </div>
+  );
+};
+
+export default App;
