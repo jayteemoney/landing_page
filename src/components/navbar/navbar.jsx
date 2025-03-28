f3c1d07eb19a201a9a05cda65d00e07aa7628584
jayteemoney <jethroirmiya@gmail.com>
2025-03-28 10:07:28 +0100

    added navbar.jsx

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri'
+import GPT from '../../assets/GPT.svg'
 import './navbar.css'
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
+    <div className='gpt3__navbar'>
+      <div className='gpt3__-links'>
+        <div className='gpt3__navbar-links_logo'>
+          <img src={GPT} alt="GPT" />
+
+        </div>
+
+      </div>
+      
     </div>
   )
 }
